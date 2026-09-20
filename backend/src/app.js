c679c2f14540cc47ad58eb64e033e60720b310dc
vikashagrahari9522-png <vikashagrahari9522@gmail.com>
2026-09-20 19:21:04 +0530

    final commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "../public")))
 app.use('/api/auth',authRoutes)
 app.use('/api/chat',chatRoutes)
 
-app.get('*', (req, res) => {
+app.get('*name', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
