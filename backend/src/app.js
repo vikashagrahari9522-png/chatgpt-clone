e61e80a8db2cc606ba5b731d854be0d61a0436ea
vikashagrahari9522-png <vikashagrahari9522@gmail.com>
2026-09-20 19:00:16 +0530

    trying

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,18 +8,14 @@ const path = require('path')
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, '../public')))
+
 app.use(cors({
-    origin: "https://chatgpt-clone-3akp.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
-app.use('/api/auth', authRoutes)
-app.use('/api/chat', chatRoutes)
-
-app.get("*name", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+app.use('/api/auth',authRoutes)
+app.use('/api/chat',chatRoutes)
 
 
 module.exports = app
