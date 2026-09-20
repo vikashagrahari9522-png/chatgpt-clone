839ac3be8d70a89fb14502e86949828350f7b46c
vikash agrahari <vikashagrahari@vikashs-MacBook-Neo.local>
2026-09-20 18:02:41 +0530

    Prepare project for Render

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ connectDb();
 
 initSocketServer(httpServer)
 
-httpServer.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
 
-})
+httpServer.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
